aec535390ac892b07b1356aeb0217851d46d4bd7
tavoralucas <lnt@cin.ufpe.br>
2019-04-11 23:34:12 -0300

    add responsive

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.car').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
         
@@ -11,7 +11,7 @@ $(document).ready(function(){
                 breakpoint: 1200,
                 settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToScroll: 1,
                 adaptiveHeight: true
                 }
             },
@@ -30,7 +30,7 @@ $(document).ready(function(){
     $('.carBrigadeiros').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
         responsive: [
@@ -38,7 +38,7 @@ $(document).ready(function(){
                 breakpoint: 1200,
                 settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToScroll: 1,
                 }
             },
             {
@@ -54,7 +54,7 @@ $(document).ready(function(){
 
 $('.border-box-contact').on('change', function() {var value = $(this).children('option:selected').val()
     if (value == 1) {
-        $('#valor').html('<img src="https://scontent-gru2-1.cdninstagram.com/vp/14bfe8fdd6edc955f0ad94114199a319/5D1C7ACB/t51.2885-15/fr/e15/s1080x1080/51583005_299873010883291_9010529138987292155_n.jpg?_nc_ht=scontent-gru2-1.cdninstagram.com" style="display:flex; position:relative; justify-content: center; width:30%; height:30%;"/>Preço: R$ 70,00')
+        $('#valor').html('<img src="https://scontent-gru2-1.cdninstagram.com/vp/14bfe8fdd6edc955f0ad94114199a319/5D1C7ACB/t51.2885-15/fr/e15/s1080x1080/51583005_299873010883291_9010529138987292155_n.jpg?_nc_ht=scontent-gru2-1.cdninstagram.com" style="display:flex; position:relative; justify-content: center; width:30%; height:30%; margin-left:10%"/>Preço: R$ 70,00')
     } else if (value == 2) {
         $('#valor').html('<img src="https://scontent-gru2-1.cdninstagram.com/vp/1d6b612046e9372ca62364a2852751b6/5D137229/t51.2885-15/e35/51852457_423299508211929_5023579258565186520_n.jpg?_nc_ht=scontent-gru2-1.cdninstagram.com" style="display:flex; position:relative; width:30%; height:30%;" /> Preço: R$ 100,00')
     } else if (value == 3) {
